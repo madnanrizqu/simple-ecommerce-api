a726102e1cdfaddc06682b7ff9fd2888d7075da6
rizquadnan <madnanrizqullah@gmail.com>
2024-03-24 07:11:46 +0700

    add specific cors

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
 // 3. Cors
-app.use(cors());
+app.use(cors({ origin: [config.get<string>("frontendUrl")] }));
 
 // 4 Logger
 if (config.get("nodeEnv") === "development") app.use(morgan("dev"));
